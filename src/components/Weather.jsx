e09c20febe8d3d5722424f96730901862dad9490
aliafzalh <aliafzalh@gmail.com>
2025-07-09 23:43:18 -0400

    2nd commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+
+
+
+// function Weather(){
+//     const [city, setCity] = useState('');
+//     const [weather, setWeather] = useState(null);
+
+// }
+
+import { useState } from "react";
+
+function Weather (){
+    const [temp, setTemp] =useState;
+
+    return (
+        <div className = "Weather">
+            New York NOw: {temp}F
+        </div>
+    )
+}
+export default Weather
